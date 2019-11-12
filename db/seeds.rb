f1ba97448d697cc939d04b6cd60d57b5bed64c27
kenave <kaverbookh@gmail.com>
2019-11-12 09:52:54 -0500

    changed seed file to extract price_range correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,9 +19,13 @@ puts "Creating loads of restaurants"
 file = File.read('db/Yelp_JSON/LV_open_restaurants_processed.json')
 data = JSON.parse(file)
 
+Place.destroy_all
+# binding.pry
+
 data["restaurants"].each do |place|
   # binding.pry
-  if !place["attributes"] == nil
+  if place["attributes"] != nil
+    # binding.pry
     Place.find_or_create_by(business_id: place["business_id"], name: place["name"], address: place["address"], city: place["city"], state: place["state"], postal_code: place["postal_code"], latitude: place["latitude"], longitude: place["longitude"], stars: place["stars"], price_range: place["attributes"]["RestaurantsPriceRange2"], categories: place["categories"])
     puts "Loaded #{place["name"]}"
   else
