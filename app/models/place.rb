fd7b4839dcb2fa714d91a19ecd4e898b1fc36f4a
Nick <nick.moi@gmail.com>
2019-11-11 23:32:22 -0500

    maps and cleaning up of some styles

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   has_many :crawl_places
   has_many :crawls, through: :crawl_places
 end
